c874bd57ee816a4698e904fd4436132e32f17727
KAMANA RAWAT <kamanarawat0908@gmail.com>
2025-09-21 11:36:00 +0530

    Add files via upload

---
+++ UserInput.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       // String input = sc.next();
+       // System.out.println(input);
+      //   double num = sc.nextDouble();
+      //  System.out.println(num);
+      //   float input = sc.nextFloat();
+        //System.out.println(input);
+         boolean input = sc.nextBoolean();
+        System.out.println(input);
+    }
+    
+}
