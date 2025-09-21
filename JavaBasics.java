5572be78b21202a05f72cee33975914995a66f29
KAMANA RAWAT <kamanarawat0908@gmail.com>
2025-09-21 11:40:04 +0530

    Add files via upload

---
+++ JavaBasics.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class JavaBasics {
+    public static void main(String []args){
+        //PATTERN
+         System.out.println("****");
+        System.out.println("***");
+         System.out.println("**");
+           System.out.println("*");
+        //VARIABLES
+        int a = 10;
+        int b= 20;
+       int perimeter = 2*(a+b);
+       System.out.println(perimeter);
+    
+
+
+    }
+}
