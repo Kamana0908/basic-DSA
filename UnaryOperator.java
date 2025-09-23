a4c5b4402d515e1cfc5ec87c374c4421dc0cb0ac
KAMANA RAWAT <kamanarawat0908@gmail.com>
2025-09-23 21:12:54 +0530

    Add files via upload

---
+++ UnaryOperator.java
@@ -0,0 +1,10 @@
+package OPERATORS;
+
+public class UnaryOperator {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = a--;
+        System.out.println(b);
+    }
+    
+}
