9e3c68fc3db0fda9d81bd5efd985b34f9dce6cf7
KAMANA RAWAT <kamanarawat0908@gmail.com>
2025-09-23 21:13:19 +0530

    Add files via upload

---
+++ RelationalOpera.java
@@ -0,0 +1,16 @@
+package OPERATORS;
+
+public class RelationalOpera {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+
+    }
+    
+}
