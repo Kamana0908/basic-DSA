686b3139fa1564cbd06c79b01d8493f309baf078
KAMANA RAWAT <kamanarawat0908@gmail.com>
2025-09-23 21:14:00 +0530

    Add files via upload

---
+++ LogicalOperat.java
@@ -0,0 +1,13 @@
+package OPERATORS;
+
+public class LogicalOperat {
+    public static void main(String[] args) {
+        // Logical AND
+        System.out.println((9>8) && (8>3));
+        // Logical OR
+        System.out.println((9<5) || (8<2));
+        // Logical Not
+        System.out.println(!(3>8));
+    }
+    
+}
