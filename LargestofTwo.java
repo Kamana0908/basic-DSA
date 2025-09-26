8a2b20b22cbf8ec59c565265f3331ed1e0d8407d
KAMANA RAWAT <kamanarawat0908@gmail.com>
2025-09-26 19:59:13 +0530

    Add files via upload

---
+++ LargestofTwo.java
@@ -0,0 +1,16 @@
+package CONDITIONAL;
+
+public class LargestofTwo {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 3;
+        if(a<b){
+            System.out.println("B is largest");
+        }else{
+            System.out.println("a is largest");
+            
+        }
+
+    }
+    
+}
