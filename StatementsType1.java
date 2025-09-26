d7b89446df7dedfa81821359e148d7cf00ad5d44
KAMANA RAWAT <kamanarawat0908@gmail.com>
2025-09-26 19:58:37 +0530

    Add files via upload

---
+++ StatementsType1.java
@@ -0,0 +1,14 @@
+package CONDITIONAL;
+
+public class StatementsType1 {
+    public static void main(String[] args) {
+        int age = 22;
+        if(age >= 18){
+            System.out.println("adult");
+
+        }else{
+            System.out.println("not adult");
+        }
+    }
+    
+}
