44e1cee74aab60a0c6714b62223859e0a9a8f57e
KAMANA RAWAT <kamanarawat0908@gmail.com>
2025-09-28 23:55:02 +0530

    Add files via upload

---
+++ Ques2.java
@@ -0,0 +1,10 @@
+package CONDITIONAL.Problems;
+
+public class Ques2 {
+    public static void main(String[] args) {
+        double temp = 103.5;
+        String report = temp >=100 ? "Fever":"No fever";
+        System.out.println(report);
+    }
+    
+}
