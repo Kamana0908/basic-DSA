bddb499284000fcc2d608dba45fc3e6810af8c9b
KAMANA RAWAT <kamanarawat0908@gmail.com>
2025-10-01 20:45:30 +0530

    Add files via upload

---
+++ loop3.java
@@ -0,0 +1,12 @@
+package FlowControls;
+
+public class loop3 {
+    public static void main(String[] args) {
+        int i = 1;
+        do{
+            System.out.println("hello");
+            i++;
+        }while(i<=10);
+    }
+    
+}
