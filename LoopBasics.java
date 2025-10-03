e210c2d85ae40791bdc863c2212d9809abfa5fdd
KAMANA RAWAT <kamanarawat0908@gmail.com>
2025-10-03 19:47:56 +0530

    Add files via upload

---
+++ LoopBasics.java
@@ -0,0 +1,16 @@
+package FlowControls;
+
+public class LoopBasics {
+    public static void main(String[] args) {
+        //While Loop
+        int counter = 0;
+        while(counter<100){
+            System.out.println("Hello");
+            counter++;
+        }
+        
+    }
+    
+
+    
+}
