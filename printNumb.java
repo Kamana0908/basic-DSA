2aa47cc7f165b58e5f13d69a529bd0aaedc547bc
KAMANA RAWAT <kamanarawat0908@gmail.com>
2025-10-04 10:36:22 +0530

    Add files via upload

---
+++ printNumb.java
@@ -0,0 +1,13 @@
+package FlowControls;
+
+public class printNumb {
+    public static void main(String[] args) {
+        int counter = 1;
+        while(counter<=10){
+            System.out.print(counter+"");
+            counter++;
+        }
+        System.out.println();
+    }
+    
+}
