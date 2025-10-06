d82c724c7376a796de30d0b2835ee68bd6ac5663
KAMANA RAWAT <kamanarawat0908@gmail.com>
2025-10-06 17:54:24 +0530

    Add files via upload

--- Ques3.java
+++ Ques3.java
@@ -1,19 +1,5 @@
-package CONDITIONAL.Problems;
-import java.util.*;
+package FlowControls.Problems;
 
 public class Ques3 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int year = sc.nextInt();
-        boolean x = (year%4) == 0;
-        boolean y = (year%100) != 0;
-        boolean z = ((year%100 == 0) && (year%400 == 0));
-        if(x && (y || z)){
-            System.out.println("Leap year");
-       
-        }else{
-            System.out.println("not a leap year");
-        }
-    }
     
 }
