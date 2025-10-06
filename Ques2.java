4cd7df296b272bab86e72af8ecbf2a8d64bf6424
KAMANA RAWAT <kamanarawat0908@gmail.com>
2025-10-06 17:53:55 +0530

    Add files via upload

--- Ques2.java
+++ Ques2.java
@@ -1,10 +1,34 @@
-package CONDITIONAL.Problems;
+package FlowControls.Problems;
+import java.util.*;
 
 public class Ques2 {
     public static void main(String[] args) {
-        double temp = 103.5;
-        String report = temp >=100 ? "Fever":"No fever";
-        System.out.println(report);
+        Scanner sc = new Scanner(System.in);
+        int num;
+        int choice;
+        int evenSum = 0;
+        int oddSum = 0;
+
+        do{
+            System.out.println("Enter the number:");
+            num = sc.nextInt();
+
+            if(num % 2 == 0){
+                evenSum += num;
+            }else{
+                oddSum += num;
+            }
+
+            System.out.println("Do you want to continue? Press 1 for yes and 0 for no:");
+
+            choice=sc.nextInt();
+
+        } while(choice==1);
+        
+        System.out.println("Sum of even num is :"+ evenSum);
+        System.out.println("Sum of odd num is :" + oddSum);
+         
     }
+
     
 }
