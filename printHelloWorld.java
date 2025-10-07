185dd8d6c0315c7f4d93ff9db76de33b4d320a52
KAMANA RAWAT <kamanarawat0908@gmail.com>
2025-10-07 10:09:46 +0530

    Add files via upload

---
+++ printHelloWorld.java
@@ -0,0 +1,12 @@
+package FNCTIONS.METHODS;
+
+public class printHelloWorld {
+    public static void printHelloWorld(){
+        System.out.println("Hello world");
+    }
+    public static void main(String[] args) {
+        printHelloWorld();//Function Call
+        
+    }  
+    
+}
