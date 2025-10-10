10d36a9b50e64a3d6aa222ea352d4ac2d8337d44
KAMANA RAWAT <kamanarawat0908@gmail.com>
2025-10-10 18:32:15 +0530

    Add files via upload

---
+++ Lecture14.java
@@ -0,0 +1,33 @@
+package FNCTIONS.OVRLOADNG;
+
+
+public class Lecture14 {
+
+    public static boolean isPrime(int n){
+    //Only for n>=2
+    //2
+    if(n==2){
+        return true;
+    }
+        
+     boolean isPrime = true;
+    for(int i=2;i<=n-1;i++){
+         if(n%i == 0){
+            isPrime = false;
+            break;
+            }
+        }
+        return isPrime;
+    }
+
+public static void main(String[] args) {
+    System.out.println(isPrime(2));
+    
+}
+
+
+            }
+
+    
+    
+
