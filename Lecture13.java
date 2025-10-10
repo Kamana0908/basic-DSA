1127a9d3e5e5fe12f39c0edc11f508ef167b1b4a
KAMANA RAWAT <kamanarawat0908@gmail.com>
2025-10-10 18:31:53 +0530

    Add files via upload

---
+++ Lecture13.java
@@ -0,0 +1,22 @@
+package FNCTIONS.OVRLOADNG;
+
+public class Lecture13 {
+    //Function to call int sum
+    public static int sum(int a, int b) {
+        return a+b;
+        
+    }
+    //Fuction to return float sum
+    public static float sum(float a, float b) {
+        return a+b;
+        
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(14,50));
+        System.out.println(sum(1.9f,4.9f));
+    }
+    
+}
+
+    
+
