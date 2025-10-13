131b3fdb4f1cc08cc59d3af3e3b9a2a1ade35fed
KAMANA RAWAT <kamanarawat0908@gmail.com>
2025-10-13 22:49:40 +0530

    Add files via upload

---
+++ paramtr.java
@@ -0,0 +1,20 @@
+package FNCTIONS.OVRLOADNG;
+
+public class paramtr {
+    //Function to calculate sum of 2 nums
+    public static int sum(int a, int b){
+
+        return a+b;
+    }
+    //Function to calculate sum of 3 nums
+    public static int sum(int a, int b, int c){
+        return a+b+c;
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(5,6));
+        System.out.println(sum(2, 7,8));
+    }
+    
+}
+    
+
