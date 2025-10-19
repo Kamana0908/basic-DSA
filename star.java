980538154c9c42c8113c58e031621d557a7c2c9d
KAMANA RAWAT <kamanarawat0908@gmail.com>
2025-10-19 22:25:57 +0530

    Add files via upload

---
+++ star.java
@@ -0,0 +1,14 @@
+package PATTERNPART1;
+
+public class star {
+    public static void main(String[] args) {
+        for(int line=1; line<=9;line++){
+            for(int j=1; j<=line; j++){
+                System.out.print("*");
+                
+            }
+            System.out.println();
+        }
+    }
+    
+}
